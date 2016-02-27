2a8aa39b9a33194516488557000564a2e9a3c068
EdwinMoq <edwin.moquete0812@gmail.com>
2016-02-27 00:23:46 -0400

    u2-common: rootdir: Fix typo.

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -15,7 +15,6 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := fstab.u2
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
-endif
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.u2.rc
